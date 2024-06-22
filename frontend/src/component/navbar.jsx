b5d96d05d8310b0e636049fb32d9ce486ef3f10d
sameet11 <sameetsabu1@gmail.com>
2024-06-22 21:06:32 +0530

    bug fixed

--- frontend/src/component/navbar.jsx
+++ frontend/src/component/navbar.jsx
@@ -2,13 +2,13 @@ import { IoIosSearch } from "react-icons/io";
 const navElements=['General','LLMs','Knowledge Base','Integrations','Data Loaders','Multi-Modal','Logic','Chat']
 const Navbar=()=>{
     return(
-        <div className='flex gap-5 pl-4 p-2 cursor-pointer'>
+        <div className='flex gap-5 pl-4 p-1 cursor-pointer'>
         <button className="hover:bg-slate-50 hover:rounded-full h-9 w-9 flex items-center justify-center">
         <IoIosSearch size={25} className="text-[#6C737F]" />
         </button>
         {navElements.map((ele,id)=>{
             return(
-                <div key={id} className={`flex items-center justify-center text-[#6C737F] text-sm font-semibold hover:text-[#9294F7] cursor-pointer ${id===0 ?'undefined':''}`}>{ele}</div>
+                <div key={id} className={`flex items-center justify-center text-[#6C737F] text-sm font-semibold hover:text-[#9294F7] cursor-pointer ${id===0 ?'underlined':''}`}>{ele}</div>
             )
         })}
         </div>
